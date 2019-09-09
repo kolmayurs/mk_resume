7f6a7b55d39cfa707ea146c9d0449587946534e9
Mayur Koli <kolimayurs@gmail.com>
2019-09-09 20:44:43 +0530

    commit

---
+++ build/precache-manifest.7e8e5c9d1035e6032293aadb5e2a268a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5700d5d738923a27f88442405c1f7f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18d8256ee2db305f2899",
+    "url": "/static/css/main.52c800b1.chunk.css"
+  },
+  {
+    "revision": "705426a28c090d63e9f4",
+    "url": "/static/js/2.dedaa825.chunk.js"
+  },
+  {
+    "revision": "18d8256ee2db305f2899",
+    "url": "/static/js/main.286de025.chunk.js"
+  },
+  {
+    "revision": "c38489cdaa3a8e84584a",
+    "url": "/static/js/runtime~main.0447cff4.js"
+  }
+]);
